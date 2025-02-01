4370323e423155732281032ab9e429453f4d80a9
riteshverma021 <riteshverma2338@gmail.com>
2025-02-01 21:51:48 +0530

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -6,7 +6,7 @@ import mongoose, { mongo } from "mongoose";
 
 
  export const dbconnected = async()=>{
-    await mongoose.connect("mongodb+srv://riteshvermaghd21:ritesh21@cluster0.zy1he.mongodb.net/jewel_item").then(()=>{
+    await mongoose.connect(process.env.DATABASE_URL).then(()=>{
         console.log("connected to db")  
     })
  } 
